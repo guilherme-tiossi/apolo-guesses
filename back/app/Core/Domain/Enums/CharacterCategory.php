7afc8a49a8b2ad089744d3c02fee2b4bcf05d03c
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2026-06-30 22:56:30 -0300

    feat: enum de categoria de personagem

---
+++ back/app/Core/Domain/Enums/CharacterCategory.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Core\Domain\Enums;
+
+enum CharacterCategory: int
+{
+    case FOOTBALL_PLAYER = 1;
+    case BASKETBALL_PLAYER = 2;
+    case MMA_FIGHTER = 3;
+    case BOXER = 4;
+    case TENNIS_PLAYER = 5;
+    case F1_DRIVER = 6;
+    case SURFER = 7;
+    case OLYMPIC_ATHLETE = 8;
+    case CHEF = 9;
+    case TV_HOST = 10;
+    case YOUTUBER = 11;
+    case INFLUENCER = 12;
+    case ACTOR = 13;
+    case ACTRESS = 14;
+    case MALE_SINGER = 15;
+    case FEMALE_SINGER = 16;
+    case COMPOSER = 17;
+    case FILM_DIRECTOR = 18;
+    case COMEDIAN = 19;
+    case POLITICIAN = 20;
+    case HISTORICAL_FIGURE = 21;
+    case MONARCH = 22;
+    case PHILOSOPHER = 23;
+    case SCIENTIST = 24;
+    case WRITER = 25;
+    case POET = 26;
+    case PAINTER = 27;
+    case SCULPTOR = 28;
+    case PHOTOGRAPHER = 29;
+    case JOURNALIST = 30;
+    case SPORTS_REPORTER = 31;
+    case ENTREPRENEUR = 32;
+    case INVESTOR = 33;
+    case TECH_CEO = 34;
+    case GAMER_STREAMER = 35;
+    case DJ = 36;
+    case MODEL = 37;
+    case FASHION_DESIGNER = 38;
+    case MILITARY = 39;
+    case RELIGIOUS_LEADER = 40;
+    case ASTRONAUT = 41;
+    case DOCTOR = 42;
+    case LAWYER = 43;
+    case FICTIONAL_CHARACTER = 44;
+    case RADIO_HOST = 45;
+    case MAGICIAN = 46;
+    case DANCER = 47;
+    case PASTRY_CHEF = 48;
+    case CARTOON_CHARACTER = 49;
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::FOOTBALL_PLAYER => 'Futebolista',
+            self::BASKETBALL_PLAYER => 'Jogador de basquete',
+            self::MMA_FIGHTER => 'Lutador de MMA',
+            self::BOXER => 'Boxeador',
+            self::TENNIS_PLAYER => 'Tenista',
+            self::F1_DRIVER => 'Piloto de Fórmula 1',
+            self::SURFER => 'Surfista',
+            self::OLYMPIC_ATHLETE => 'Atleta olímpico',
+            self::CHEF => 'Cozinheiro / Chef',
+            self::TV_HOST => 'Apresentador de TV',
+            self::YOUTUBER => 'Youtuber',
+            self::INFLUENCER => 'Influenciador',
+            self::ACTOR => 'Ator',
+            self::ACTRESS => 'Atriz',
+            self::MALE_SINGER => 'Cantor',
+            self::FEMALE_SINGER => 'Cantora',
+            self::COMPOSER => 'Compositor',
+            self::FILM_DIRECTOR => 'Diretor de cinema',
+            self::COMEDIAN => 'Comediante / Humorista',
+            self::POLITICIAN => 'Político',
+            self::HISTORICAL_FIGURE => 'Figura histórica',
+            self::MONARCH => 'Rei / Imperador',
+            self::PHILOSOPHER => 'Filósofo',
+            self::SCIENTIST => 'Cientista',
+            self::WRITER => 'Escritor',
+            self::POET => 'Poeta',
+            self::PAINTER => 'Pintor',
+            self::SCULPTOR => 'Escultor',
+            self::PHOTOGRAPHER => 'Fotógrafo',
+            self::JOURNALIST => 'Jornalista',
+            self::SPORTS_REPORTER => 'Repórter esportivo',
+            self::ENTREPRENEUR => 'Empresário',
+            self::INVESTOR => 'Investidor',
+            self::TECH_CEO => 'CEO de tech',
+            self::GAMER_STREAMER => 'Gamer / streamer',
+            self::DJ => 'DJ',
+            self::MODEL => 'Modelo',
+            self::FASHION_DESIGNER => 'Designer de moda',
+            self::MILITARY => 'Militar',
+            self::RELIGIOUS_LEADER => 'Líder religioso',
+            self::ASTRONAUT => 'Astronauta',
+            self::DOCTOR => 'Médico',
+            self::LAWYER => 'Advogado',
+            self::FICTIONAL_CHARACTER => 'Personagem fictício',
+            self::RADIO_HOST => 'Apresentador de rádio',
+            self::MAGICIAN => 'Mágico / ilusionista',
+            self::DANCER => 'Dançarino',
+            self::PASTRY_CHEF => 'Chef de confeitaria',
+            self::CARTOON_CHARACTER => 'Personagem de desenho animado',
+        };
+    }
+}
--- back/database/seeders/CharacterCategorySeeder.php
+++ back/database/seeders/CharacterCategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Core\Domain\Enums\CharacterCategory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -9,63 +10,11 @@ class CharacterCategorySeeder extends Seeder
 {
     public function run(): void
     {
-        $categories = [
-            1  => 'Futebolista',
-            2  => 'Jogador de basquete',
-            3  => 'Lutador de MMA',
-            4  => 'Boxeador',
-            5  => 'Tenista',
-            6  => 'Piloto de Fórmula 1',
-            7  => 'Surfista',
-            8  => 'Atleta olímpico',
-            9  => 'Cozinheiro / Chef',
-            10 => 'Apresentador de TV',
-            11 => 'Youtuber',
-            12 => 'Influenciador',
-            13 => 'Ator',
-            14 => 'Atriz',
-            15 => 'Cantor',
-            16 => 'Cantora',
-            17 => 'Compositor',
-            18 => 'Diretor de cinema',
-            19 => 'Comediante / Humorista',
-            20 => 'Político',
-            21 => 'Figura histórica',
-            22 => 'Rei / Imperador',
-            23 => 'Filósofo',
-            24 => 'Cientista',
-            25 => 'Escritor',
-            26 => 'Poeta',
-            27 => 'Pintor',
-            28 => 'Escultor',
-            29 => 'Fotógrafo',
-            30 => 'Jornalista',
-            31 => 'Repórter esportivo',
-            32 => 'Empresário',
-            33 => 'Investidor',
-            34 => 'CEO de tech',
-            35 => 'Gamer / streamer',
-            36 => 'DJ',
-            37 => 'Modelo',
-            38 => 'Designer de moda',
-            39 => 'Militar',
-            40 => 'Líder religioso',
-            41 => 'Astronauta',
-            42 => 'Médico',
-            43 => 'Advogado',
-            44 => 'Personagem fictício',
-            45 => 'Apresentador de rádio',
-            46 => 'Mágico / ilusionista',
-            47 => 'Dançarino',
-            48 => 'Chef de confeitaria',
-            49 => 'Personagem de desenho animado',
-        ];
-
-        foreach ($categories as $id => $name) {
+        foreach (CharacterCategory::cases() as $category) {
             DB::table('character_categories')->updateOrInsert([
-                'id' => $id,
+                'id' => $category->value,
             ], [
-                'name' => $name,
+                'name' => $category->label(),
             ]);
         }
     }
