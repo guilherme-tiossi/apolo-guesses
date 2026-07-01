f3a4188a99a9710d551bed54cd22a82884cdfc80
guilherme-tiossi <guilherme-tiossi>
2026-06-30 23:26:37 -0300

    feat: adicionado atributos

---
+++ back/database/migrations/2026_06_29_232500_create_attributes_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attribute_subgroup_id')->constrained();
+            $table->string('question');
+            $table->string('portuguese_question');
+            $table->unique(['attribute_subgroup_id', 'question']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('attributes');
+    }
+};
