2757983bc76a47209a6026ad56527fd19ff3db51
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2026-06-30 22:53:42 -0300

    feat: adicionado enums de grupos e subgrupos de atributos

--- back/database/seeders/AttributeGroupSeeder.php
+++ back/database/seeders/AttributeGroupSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Core\Domain\Enums\AttributeGroup;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -9,28 +10,11 @@ class AttributeGroupSeeder extends Seeder
 {
     public function run(): void
     {
-        $groups = [
-            1 => 'appearance',
-            2 => 'identity',
-            4 => 'personality',
-            5 => 'career',
-            9 => 'sports',
-            10 => 'entertainment',
-            12 => 'hobbies',
-            14 => 'accomplishments',
-            16 => 'history',
-            17 => 'geography',
-            18 => 'technology',
-            19 => 'fiction',
-            20 => 'powers',
-            24 => 'miscellaneous',
-        ];
-
-        foreach ($groups as $id => $name) {
+        foreach (AttributeGroup::cases() as $group) {
             DB::table('attribute_groups')->updateOrInsert([
-                'id' => $id,
+                'id' => $group->value,
             ], [
-                'name' => $name,
+                'name' => $group->label(),
             ]);
         }
     }
