223a5ff2cd523a0ac3cbba9d952eab0220741adc
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2026-07-06 22:05:54 -0300

    feat: adicionado questões iniciais

---
+++ back/app/Core/Domain/Enums/InitialQuestion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Core\Domain\Enums;
+
+enum InitialQuestion: string
+{
+    case SKIN_FAIR = 'skin_fair';
+    case SKIN_DARK = 'skin_dark';
+    case HAIR_STRAIGHT = 'hair_straight';
+    case AGE_ADULT = 'age_adult';
+    case GENDER_MALE = 'gender_male';
+    case GENDER_FEMALE = 'gender_female';
+    case NATIONALITY_BRAZILIAN = 'nationality_brazilian';
+    case NATIONALITY_FICTICIONAL = 'nationality_fictional';
+    case LIVING_ALIVE = 'living_alive';
+    case LIVING_DECEASED = 'living_deceased';
+    case MEDIA_DIGITAL_CONTENT = 'media_digital_content';
+}
