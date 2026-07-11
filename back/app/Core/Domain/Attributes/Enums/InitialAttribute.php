ebb0eed022ca300fb582d3d992456c24377ab5b0
guilherme-tiossi <guilherme-tiossi>
2026-07-11 18:30:02 -0300

    fix: melhorias e correções de bugs

--- back/app/Core/Domain/Attributes/Enums/InitialAttribute.php
+++ back/app/Core/Domain/Attributes/Enums/InitialAttribute.php
@@ -9,7 +9,14 @@ enum InitialAttribute: string implements Attribute
     case SKIN_FAIR = 'skin_fair';
     case SKIN_DARK = 'skin_dark';
     case HAIR_STRAIGHT = 'hair_straight';
+    case HAIR_WAVY = 'hair_wavy';
+    case HAIR_CURLY = 'hair_curly';
+    case HAIR_COILY = 'hair_coily';
     case AGE_ADULT = 'age_adult';
+    case AGE_CHILD = 'age_child';
+    case AGE_TEENAGER = 'age_teenager';
+    case AGE_OVER_FORTY = 'age_over_forty';
+    case AGE_ELDERLY = 'age_elderly';
     case GENDER_MALE = 'gender_male';
     case GENDER_FEMALE = 'gender_female';
     case NATIONALITY_BRAZILIAN = 'nationality_brazilian';
--- back/app/Core/Domain/Attributes/Enums/SecondaryAttribute.php
+++ back/app/Core/Domain/Attributes/Enums/SecondaryAttribute.php
@@ -6,13 +6,6 @@ use App\Core\Domain\Attributes\Interfaces\Attribute;
 
 enum SecondaryAttribute: string implements Attribute
 {
-    case HAIR_WAVY = 'hair_wavy';
-    case HAIR_CURLY = 'hair_curly';
-    case HAIR_COILY = 'hair_coily';
-    case AGE_CHILD = 'age_child';
-    case AGE_TEENAGER = 'age_teenager';
-    case AGE_OVER_FORTY = 'age_over_forty';
-    case AGE_ELDERLY = 'age_elderly';
     case RELIGION_IMPORTANT = 'religion_important';
     case RELIGION_CHRISTIAN = 'religion_christian';
     case RELIGION_MUSLIM = 'religion_muslim';
