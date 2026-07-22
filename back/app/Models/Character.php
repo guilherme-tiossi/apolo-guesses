68a2b13403b4f34bac9e874ec4c7d21a442b9b47
guilherme-tiossi <guilherme-tiossi>
2026-07-21 22:50:17 -0300

    feat: ajustes e melhorias em seeder

--- back/app/Models/Character.php
+++ back/app/Models/Character.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name', 'picture', 'character_category_id'])]
 class Character extends Model
@@ -12,5 +13,10 @@ class Character extends Model
     public function characterCategory(): BelongsTo
     {
         return $this->belongsTo(CharacterCategory::class);
+    }
+
+    public function attributes(): HasMany
+    {
+        return $this->hasMany(CharacterAttribute::class);
     }
 }
